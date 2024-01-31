f4fa8e166164079dc95d789c1e19db5c0c3af804
Dobberman <pangestubima89@gmail.com>
2024-01-31 20:37:05 +0700

    add icon infinite carousell

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        "syne": "Syne",
-      },
-      backgroundImage: {
-        "main": "url('/public/image/background.png')",
-        "grad": "url('/public/image/gradient.png')",
-      }
+    content: ['**/*.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                syne: 'Syne',
+            },
+            backgroundImage: {
+                main: "url('/public/image/background.png')",
+                grad: "url('/public/image/gradient.png')",
+            },
+            animation: {
+                'infinite-scroll': 'infinite-scroll 25s linear infinite',
+            },
+            keyframes: {
+                'infinite-scroll': {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(-100%)' },
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
-
